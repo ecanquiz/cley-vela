1c77fbd783797d5b8eac50542d088566c4283bb6
Ernesto Canquiz <fdsoil123@gmail.com>
2023-07-27 20:28:35 -0400

    chore: Reconfig

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  css: [`assets/styles/main.min.css`],
   devtools: { enabled: true },
     modules: [
     '@nuxtjs/tailwindcss',
@@ -9,18 +10,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'stylesheet', type:"text/css", href: '/assets/styles/main.min.css' },
         { rel: 'stylesheet', type:"text/css", href: 'https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' },
-        
-        
-        
-        
-        
       ]
     }
-    
-    
-      
-    
  }
 })
